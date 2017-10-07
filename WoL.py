1d515f7a3bf397ed5fc82259badd647aec9b1e5e
JStuve <stuve.josh@gmail.com>
2017-10-07 23:18:15 +0000

    Fixed config issues

--- WoL.py
+++ WoL.py
@@ -6,13 +6,14 @@ Created on Fri Oct  6 18:39:20 2017
 @author: joshuastuve
 """
 
+import sys
 import config
 from wakeonlan import wol
 
 ''' Needs to return message for user '''
 def WakeUpComputer():
     try:
-        wol.send_magic_packet(config.BlackBeauty)
+        wol.send_magic_packet(config.BlackBeauty_MAC)
         return( config.name + ", I have awoken " + config.CompName + ".")
     except:
-        return( config.name + ", there was an issue processing WoL.. Sorry.")
+        return( config.name + ", there was an issue processing WoL.. Sorry.. .." + str(sys.exc_info()))
